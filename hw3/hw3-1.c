9b1d8cb5972b9110a8ad2c7da0d43c2b9c3d3492
YU SHAO-HSIEN <72987911+ceeyu@users.noreply.github.com>
2024-03-31 16:37:21 +0800

    Create hw3-1.c

---
+++ hw3/hw3-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void replace_all(char *str, char oldChar, char newChar) {
+  int i;
+  for (i = 0; str[i] != '\0'; i++) {
+    if (str[i] == oldChar) {
+      str[i] = newChar;
+    }
+  }
+}
+
+int main() {
+  char str[100], oldChar = 'a', newChar = 'A';
+
+  printf("Enter any string: ");
+  scanf("%s", str);
+
+  printf("\nString before replacing: \n%s\n", str);
+  replace_all(str, oldChar, newChar);
+  printf("\nString after replacing %c with %c :\n%s",oldChar,newChar,str);
+ 
+  return 0;
+}
